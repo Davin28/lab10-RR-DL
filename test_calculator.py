bf4ee97973541bf5f83773edbc8e34128b12c871
David <leigued2854@gmail.com>
2025-04-14 23:12:56 -0400

    completed it i think try 1

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,9 @@
+# https://github.com/Davin28/lab10-RR-DL
+# Partner 1: David Leigue
+# Partner 2: Renan Regalado
 import math
 import unittest
-from calculator import add, sub, mul, div, log, exp
+from calculator import add, subtract, mul, div, logarithm, hypotenuse, square_root
 
 class TestCalculator(unittest.TestCase):
     # Partner 2
@@ -10,21 +13,21 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self): # 3 assertions
-        self.assertEqual(sub(5, 3), 2)
-        self.assertEqual(sub(0, 5), -5)
-        self.assertEqual(sub(-2, -3), 1)
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 5), -5)
+        self.assertEqual(subtract(-2, -3), 1)
     ##########################
 
     # Partner 1
-    def test_multiply(self):  # 3 assertions
-        self.assertEqual(mul(2, 3), 6)
-        self.assertEqual(mul(-1, 4), -4)
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(-2, 5), -10)
         self.assertEqual(mul(0, 100), 0)
 
-    def test_divide(self):  # 3 assertions
-        self.assertAlmostEqual(div(2, 10), 5.0)
-        self.assertAlmostEqual(div(1, 3), 3.0)
-        self.assertAlmostEqual(div(-2, 4), -2.0)
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(2, 10), 5)
+        self.assertEqual(div(1, 5), 5)
+        self.assertAlmostEqual(div(0.5, 2), 4)
     ##########################
 
     # Partner 2
@@ -33,28 +36,28 @@ class TestCalculator(unittest.TestCase):
             div(0, 5)
 
     def test_logarithm(self): # 3 assertions
-        self.assertEqual(log(2, 8), 3)
-        self.assertEqual(log(10, 100), 2)
-        self.assertEqual(log(3, 27), 3)
+        self.assertEqual(logarithm(2, 8), 3)
+        self.assertEqual(logarithm(10, 100), 2)
+        self.assertEqual(logarithm(3, 27), 3)
 
     def test_log_invalid_base(self): # 1 assertion
         with self.assertRaises(ValueError):
-            log(1, 5)  # log base 1 is invalid
+            logarithm(1, 5)  # log base 1 is invalid
     ##########################
-    
+
     # Partner 1
     def test_log_invalid_argument(self):  # 1 assertion
         with self.assertRaises(ValueError):
-            log(10, -5)  # log of a negative number is invalid
+            logarithm(10, -5)  # log of a negative number is invalid
 
     def test_hypotenuse(self):  # 3 assertions
-        self.assertAlmostEqual(math.hypot(3, 4), 5.0)
-        self.assertAlmostEqual(math.hypot(5, 12), 13.0)
-        self.assertAlmostEqual(math.hypot(0, 7), 7.0)
+        self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13.0)
+        self.assertAlmostEqual(hypotenuse(0, 7), 7.0)
 
     def test_sqrt(self):  # 3 assertions
-        self.assertAlmostEqual(math.sqrt(4), 2.0)
-        self.assertAlmostEqual(math.sqrt(9), 3.0)
+        self.assertAlmostEqual(square_root(4), 2.0)
+        self.assertAlmostEqual(square_root(9), 3.0)
         with self.assertRaises(ValueError):
             math.sqrt(-1)
     ##########################
